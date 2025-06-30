d8eb60aac88715013e01e58dc9750e671891f59e
Mohit Thakur <techformation.mohit@gmail.com>
2025-06-30 16:52:44 +0530

    SignIn and SignUp Form/ Login page created..

---
+++ src/components/Login.js
@@ -0,0 +1,54 @@
+import React, { use } from "react";
+import Header from "./Header";
+import { useState } from "react";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInFrom = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img src="/netflix-bg-img.jpg" alt="Netflix-logo" />
+      </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-85">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 rounded-lg bg-red-700 w-full">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInFrom}>
+          {isSignInForm
+            ? "New to Netflix? Sing Up Now"
+            : "Already registered? Sing In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
